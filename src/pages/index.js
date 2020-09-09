f81c4546b6ee80134cd46d497a60c2c18a87e900
Luke Bennett <hello@lukebennett.com.au>
2020-09-09 11:42:48 +1000

    Remove most content from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
-import { Layout, SEO, Sidebar, Contact } from '../components';
+import { Layout, SEO } from '../components';
 
 function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
-      <div className="flex-1 w-full max-w-3xl px-6 mx-auto lg:max-w-7xl">
-        <div className="grid py-12 border-gray-100 lg:grid-cols-3 lg:gap-16">
-          <Sidebar />
-          <div className="col-span-2 space-y-12">
-            <Contact />
-          </div>
-        </div>
-      </div>
+      <h1 className="py-24 text-5xl font-black text-center text-slate font-display">
+        Blue Cow Gelato
+      </h1>
     </Layout>
   );
 }
