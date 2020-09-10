fdc6fa14e7f46be269b3de1ff8f15c3e7579b94a
Luke Bennett <hello@lukebennett.com.au>
2020-09-10 17:31:06 +1000

    Remove deleted file

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export * from './contact-form';
 export * from './contact-header';
 export * from './business-logo';
-export * from './contact';
 export * from './layout';
 export * from './seo';
 export * from './sidebar';
