8a4386cbe899f15e99b7fa322b153ddae26acca0
Luke Bennett <hello@lukebennett.com.au>
2020-09-10 17:31:25 +1000

    Minor formatting

--- src/components/instagram-widget.js
+++ src/components/instagram-widget.js
@@ -9,46 +9,48 @@ function InstagramWidget() {
   const instagramFeed = useInstagram();
 
   return (
-    <div className="p-4 mx-auto overflow-hidden max-w-7xl lg:p-20">
-      <div className="py-8 pb-16 text-center ">
-        <a
-          href={siteMetadata.social.instagram}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="p-0 px-2 py-2 text-3xl font-black text-white uppercase md:text-5xl lg:text-6xl font-display bg-sky-blue">
-            #bluecowgelato
-          </h2>
-        </a>
-      </div>
-      <div className="grid grid-cols-2 col-span-3 gap-4 md:grid-cols-3 lg:grid-cols-5">
-        {instagramFeed.map((feed, index) => (
-          <div
-            key={index}
-            className={index === instagramFeed.length - 1 && 'lg:hidden'}
+    <div className="px-4 mx-auto overflow-hidden bg-white sm:px-6 lg:px-8">
+      <div className="py-8 pb-16 mx-auto space-y-16 max-w-7xl">
+        <div className="text-center">
+          <a
+            href={siteMetadata.social.instagram}
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <a
-              href={feed.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="relative block h-0 overflow-hidden aspect-ratio-square"
+            <h2 className="p-0 px-2 py-2 text-3xl font-black text-white uppercase md:text-5xl lg:text-6xl font-display bg-sky-blue">
+              #bluecowgelato
+            </h2>
+          </a>
+        </div>
+        <div className="grid grid-cols-2 col-span-3 gap-4 md:grid-cols-3 lg:grid-cols-5">
+          {instagramFeed.map((feed, index) => (
+            <div
+              key={index}
+              className={index === instagramFeed.length - 1 && 'lg:hidden'}
             >
-              <img
-                key={index}
-                src={feed.src}
-                alt={feed.caption}
-                srcSet={feed.srcSet.join(', ')}
-                className="object-contain w-full h-full"
-              />
-              <div
-                aria-hidden
-                className="absolute inset-0 p-4 overflow-y-auto text-white break-words transition duration-300 ease-in-out bg-black bg-opacity-50 opacity-0 overscroll-y-auto hover:opacity-100"
+              <a
+                href={feed.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative block h-0 overflow-hidden aspect-ratio-square"
               >
-                {feed.caption || 'View on Instagram'}
-              </div>
-            </a>
-          </div>
-        ))}
+                <img
+                  key={index}
+                  src={feed.src}
+                  alt={feed.caption}
+                  srcSet={feed.srcSet.join(', ')}
+                  className="object-contain w-full h-full"
+                />
+                <div
+                  aria-hidden
+                  className="absolute inset-0 p-4 overflow-y-auto text-white break-words transition duration-300 ease-in-out bg-black bg-opacity-50 opacity-0 overscroll-y-auto hover:opacity-100"
+                >
+                  {feed.caption || 'View on Instagram'}
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
