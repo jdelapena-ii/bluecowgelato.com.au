473466793b51cd69a1027f235fed894a4d120883
Jedo <jtdelapenaii@gmail.com>
2020-09-11 08:58:03 +0800

    About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import GatsbyImage from 'gatsby';
+import PropTypes from 'prop-types';
 
-import { Layout, SEO } from '../components';
+import { Layout, SEO, ContactForm } from '../components';
 
 function AboutPage() {
   return (
@@ -9,8 +11,21 @@ function AboutPage() {
       <h1 className="py-48 text-5xl font-black text-center text-slate font-display">
         About
       </h1>
+      <ContactForm />
     </Layout>
   );
 }
+
+function AboutHero({ image }) {
+  return (
+    <div className="absolute inset-0 flex">
+      <GatsbyImage fluid={image} className="flex-1" />
+    </div>
+  );
+}
+
+AboutHero.propTypes = {
+  image: PropTypes.node.isRequired,
+};
 
 export default AboutPage;
