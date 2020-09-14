8e11b48b355591936b1325bf90e0a0f0fd218dea
Jedo <jtdelapenaii@gmail.com>
2020-09-14 15:35:26 +0800

    Blue Cow Gelato Cart section. Need to fix image rounded border :D

--- src/components/text-with-image.js
+++ src/components/text-with-image.js
@@ -2,17 +2,12 @@ import React from 'react';
 import GatsbyImage from 'gatsby-image';
 import PropTypes from 'prop-types';
 
-function TextWithImage({
-  aspectRatio = 'aspect-ratio-square',
-  image,
-  children,
-  reverse,
-}) {
+function TextWithImage({ image, children, reverse }) {
   function Image() {
     return (
-      <div className="relative h-full">
-        <div className="absolute inset-0 flex">
-          <GatsbyImage fluid={image} className="flex-1" />
+      <div className="relative h-full border-8 border-gray-100">
+        <div className="inset-0 flex">
+          <GatsbyImage fluid={image} className="flex-1 p-10" />
         </div>
       </div>
     );
@@ -20,14 +15,19 @@ function TextWithImage({
   return (
     <article className="relative grid items-center w-full max-w-xl gap-12 px-4 mx-auto lg:max-w-none lg:grid-cols-2 sm:px-6 lg:px-8">
       {reverse && <Image />}
-
+      <div className="flex flex-col justify-center break-words lg:mx-auto">
+        {children && (
+          <h3 className="mt-4 text-3xl leading-none tracking-normal text-right text-gray-100 uppercase font-display sm:tracking-normal sm:text-3xl">
+            {children}
+          </h3>
+        )}
+      </div>
       {!reverse && <Image />}
     </article>
   );
 }
 
 TextWithImage.propTypes = {
-  aspectRatio: PropTypes.string,
   children: PropTypes.node.isRequired,
   image: PropTypes.object.isRequired,
   reverse: PropTypes.bool,
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,9 +18,17 @@ function IndexPage() {
 function GelatoCart() {
   const { cartImage } = useGraphQL();
   return (
-    <article className="px-4 pt-48 pb-16 sm:px-6 lg:px-8">
+    <article className="px-4 pt-24 pb-24 sm:px-6 lg:px-8">
       <div className="w-full max-w-4xl mx-auto space-y-4 sm:space-y-0 sm:space-x-12 sm:flex">
-        <TextWithImage image={cartImage.childImageSharp.fluid} reverse />
+        <TextWithImage
+          image={cartImage.childImageSharp.fluid}
+          reverse
+        >
+          <h1 className="font-bold sm:text-6xl">Blue Cow Gelato Cart</h1>
+          <p className="sm:text-3xl">Available for Hire</p>
+          <p className="sm:text-3xl">Parties . Events</p>
+          <p className="sm:text-3xl">Weddings . Markets</p>
+        </TextWithImage>
       </div>
     </article>
   );
