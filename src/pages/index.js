baad5d62f4cca0a1919f4b19418338a23a5c6f70
Jedo <jtdelapenaii@gmail.com>
2020-09-14 15:42:46 +0800

    ESLint fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,10 +20,7 @@ function GelatoCart() {
   return (
     <article className="px-4 pt-24 pb-24 sm:px-6 lg:px-8">
       <div className="w-full max-w-4xl mx-auto space-y-4 sm:space-y-0 sm:space-x-12 sm:flex">
-        <TextWithImage
-          image={cartImage.childImageSharp.fluid}
-          reverse
-        >
+        <TextWithImage image={cartImage.childImageSharp.fluid} reverse>
           <h1 className="font-bold sm:text-6xl">Blue Cow Gelato Cart</h1>
           <p className="sm:text-3xl">Available for Hire</p>
           <p className="sm:text-3xl">Parties . Events</p>
