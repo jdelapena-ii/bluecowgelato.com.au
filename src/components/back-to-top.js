626d657bf3374bf7ebca27f87b157a887ae815a6
Luke Bennett <hello@lukebennett.com.au>
2020-09-16 20:45:50 +1000

    Hide on mobile

--- src/components/back-to-top.js
+++ src/components/back-to-top.js
@@ -4,8 +4,8 @@ import { BackToTopIcon } from './vectors';
 
 function BackToTop() {
   return (
-    <div className="fixed inset-0 flex pointer-events-none">
-      <div className="flex items-end justify-end flex-1 w-full px-4 py-12 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+    <div className="fixed inset-0 hidden pointer-events-none md:flex">
+      <div className="flex items-end justify-end flex-1 w-full px-4 py-24 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <a href="#main-content" className="rounded-full pointer-events-auto">
           <BackToTopIcon
             style={{
