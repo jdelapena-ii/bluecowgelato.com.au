b8aa68338ab7e1596c71b3bef6cd724e12e871df
Luke Bennett <hello@lukebennett.com.au>
2020-09-16 18:15:34 +1000

    Clean up markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,74 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
 import {
   Layout,
   SEO,
-  InstagramWidget,
+  HeroCarousel,
+  HeadingWithText,
   About,
   TextWithImage,
   Contact,
-  HeadingWithText,
+  InstagramWidget,
 } from '../components';
-import { Hero } from '../components/hero-carousel';
+
 import { useGraphQL } from '../hooks';
 
 function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
-      <Hero />
-      <HeadingWithText
-        heading={
-          <>
-            <h1 className="text-4xl font-black text-white uppercase sm:text-5xl md:text-6xl font-display">
-              Artisan Style Gelato
-            </h1>
-          </>
-        }
-        body={
-          <>
-            <p>
-              Locally owned and managed, this Artisan Gelateria makes its own
-              gelato by hand with the aid of its specialist Italian machine.
-            </p>
-            <p>
-              Using real ingredients, the flavours change constanly depending on
-              what's in season and what's popular. NO artificial colours,
-              flavours or preservatives are used. Vegan, gluten free and dairy
-              free flavours are also available.
-            </p>
-            <p>
-              So do your taste buds a favour and be blown away by the refreshing
-              real flavours!
-            </p>
-          </>
-        }
-      />
-
-      <CustomCakes bgColorClass="bg-slate" />
-      <About>
-        <h1 className="text-5xl text-center text-slate font-dots">About</h1>
-        <p className="mt-10 prose text-center text-slate">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-          maiores architecto sequi odio consectetur, harum neque modi magnam
-          quos sapiente possimus nisi eum. Ducimus sit sint ab dolor corrupti
-          fugit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Recusandae maiores architecto sequi odio consectetur, harum neque modi
-          magnam quos sapiente possimus nisi eum. Ducimus sit sint ab dolor
-          corrupti fugit.
-        </p>
-
-        <div className="flex justify-center mt-8">
-          <Link
-            to="/about/"
-            className="px-4 text-3xl font-bold tracking-wide uppercase transition-all duration-300 ease-in-out transform bg-white rounded-md md:text-4xl text-slate font-display hover:text-white hover:bg-slate hover:scale-90"
-          >
-            Read more
-          </Link>
-        </div>
-      </About>
+      <HeroCarousel />
+      <ArtisanStyleGelato />
+      <CustomCakes />
+      <AboutSection />
       <GelatoCart />
       <Contact bgColorClass="bg-gold" />
       <InstagramWidget />
@@ -76,7 +29,37 @@ function IndexPage() {
   );
 }
 
-function CustomCakes({ bgColorClass }) {
+function ArtisanStyleGelato() {
+  return (
+    <HeadingWithText
+      heading={
+        <h1 className="text-4xl font-black text-white uppercase sm:text-5xl md:text-6xl font-display">
+          Artisan Style Gelato
+        </h1>
+      }
+      body={
+        <>
+          <p>
+            Locally owned and managed, this Artisan Gelateria makes its own
+            gelato by hand with the aid of its specialist Italian machine.
+          </p>
+          <p>
+            Using real ingredients, the flavours change constanly depending on
+            what's in season and what's popular. NO artificial colours, flavours
+            or preservatives are used. Vegan, gluten free and dairy free
+            flavours are also available.
+          </p>
+          <p>
+            So do your taste buds a favour and be blown away by the refreshing
+            real flavours!
+          </p>
+        </>
+      }
+    />
+  );
+}
+
+function CustomCakes() {
   const { cakeImage } = useGraphQL();
   return (
     <TextWithImage image={cakeImage.childImageSharp.fluid}>
@@ -96,6 +79,35 @@ function CustomCakes({ bgColorClass }) {
         </div>
       </div>
     </TextWithImage>
+  );
+}
+
+function AboutSection() {
+  return (
+    <About>
+      <div className="space-y-8 text-center">
+        <h2 className="text-5xl text-slate font-dots">About</h2>
+        <div className="font-medium prose text-slate">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            maiores architecto sequi odio consectetur, harum neque modi magnam
+            quos sapiente possimus nisi eum. Ducimus sit sint ab dolor corrupti
+            fugit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Recusandae maiores architecto sequi odio consectetur, harum neque
+            modi magnam quos sapiente possimus nisi eum. Ducimus sit sint ab
+            dolor corrupti fugit.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <Link
+            to="/about/"
+            className="px-4 py-1 text-2xl font-bold tracking-wide uppercase transition-all duration-300 ease-in-out transform bg-white rounded-md md:text-3xl text-slate font-display hover:text-white hover:bg-slate hover:scale-90"
+          >
+            Read more
+          </Link>
+        </div>
+      </div>
+    </About>
   );
 }
 
