711ea77af8d53f084cd9b84743f7f098f523e50c
Luke Bennett <hello@lukebennett.com.au>
2020-09-28 13:46:01 +1000

    Style menu page to match mockup

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-import { Layout, SEO, Menu, LandingMenu } from '../components';
+import {
+  Layout,
+  SEO,
+  Menu,
+  Contact,
+  InstagramWidget,
+  BusinessLogos,
+} from '../components';
 
 function MenuPage() {
   return (
     <Layout>
       <SEO title="Menu" />
-      <div aria-hidden className="pt-48 bg-gold" />
-      <LandingMenu />
+      <div aria-hidden className="pt-48 bg-slate" />
       <Menu />
+      <Contact bgColorClass="bg-gold" />
+      <InstagramWidget />
+      <BusinessLogos />
     </Layout>
   );
 }
