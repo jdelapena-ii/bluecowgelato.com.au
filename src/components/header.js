816167c7d01ea2d6841e0d3e9e481e426cf46cc0
Luke Bennett <hello@lukebennett.com.au>
2020-09-28 10:25:28 +1000

    Remove console.log

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, useEffect } from 'react';
+import React, { useState, Fragment } from 'react';
 import { Link } from 'gatsby';
 
 import { useGraphQL, useSiteContext } from '../hooks';
@@ -14,10 +14,6 @@ function Header() {
   const { isHeaderActive } = useSiteContext();
 
   const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    console.log(isHeaderActive);
-  }, [isHeaderActive]);
 
   return (
     <nav
