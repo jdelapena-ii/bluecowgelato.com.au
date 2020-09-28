cf5dd7fd4c0b6c3cf85139200ebcdd55a2274f07
Luke Bennett <hello@lukebennett.com.au>
2020-09-28 16:20:19 +1000

    Style 404 and success pages

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import { Layout, SEO } from '../components';
+import {
+  Layout,
+  SEO,
+  HeadingWithText,
+  BusinessLogos,
+  InstagramWidget,
+} from '../components';
 
 function SuccessPage() {
   return (
     <Layout>
       <SEO title="Success" />
-      <article className="px-4 pt-12 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-center text-gray-900">
-          Success
-        </h1>
-        <p className="text-center lead">
-          Thank you for your message. We will get back to you shortly.
-        </p>
-        <div className="mt-6 text-center">
-          <Link
-            to="/"
-            className="inline-block rounded-none bg-gray-800 px-4 py-2.5 text-base leading-6 font-semibold text-white focus:border-gray-900 hover:bg-gray-700 active:bg-gray-900"
-          >
-            Return home
-          </Link>
-        </div>
+      <article className="pt-48 pb-12">
+        <HeadingWithText
+          heading={
+            <h1 className="flex-1 text-4xl font-black text-white uppercase sm:text-5xl md:text-6xl font-display">
+              Success
+            </h1>
+          }
+        >
+          <div className="space-y-6">
+            <div className="prose text-white">
+              <p>
+                Thank you for your message. We will get back to you shortly.
+              </p>
+            </div>
+            <p>
+              <Link
+                to="/"
+                className="px-6 py-2 mx-auto text-xl text-white uppercase rounded-md bg-slate font-dots"
+              >
+                Return home
+              </Link>
+            </p>
+          </div>
+        </HeadingWithText>
       </article>
+      <InstagramWidget />
+      <BusinessLogos />
     </Layout>
   );
 }
